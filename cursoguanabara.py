71eee088301ac5272684e2660387e6cfaf7dd613
Jadson Alves <jadson.almeida@escolar.ifrn.edu.br>
2025-06-06 19:23:35 -0300

    cursoguanabara.py

---
+++ cursoguanabara.py
@@ -0,0 +1,32 @@
+"""n1 = int(input("digite um numero: "))
+n2 = int(input("digite outro numero: "))
+
+soma = n1 + n2
+
+print("soma entre {} e {} vale {}".format(n1, n2, soma))"""
+#####FIm
+
+"""a = (input("digite algo: "))
+print("O tipo primitivo desse valor é: ", type(a) )
+print("Só tem espaços?: ", a.isspace())
+print("É um numero?: ", a.isnumeric())
+print("É alfabeto?: ", a.isalpha())
+print("É alfanumerico?: ", a.isalnum())
+print("Esta em maiúsculas?: ", a.isupper())
+print("Está capitaizado?: ", a.istitle())"""
+######################################################
+#Operadores Aritiméticos
+
+"""nome=input("digite seu nome: ")
+print("Prazer em te conhecer:{:=^15}! ".format(nome))"""
+
+"""n1= int(input("Digite um valor: "))
+n2=int(input("Digite outro valor: "))
+s=n1+n2
+m=n1*n2
+d=n1/n2
+di=n1//n2
+e=n1**n2
+
+print("A soma vale {}, o produto é {} e a divisão é {:.3}".format(s, m, d), end=" ")
+print("divisão inteira é {} e pontencia {}".format(di, e))"""
