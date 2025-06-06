e7ae1771bd910a8927957bfc93c618f0b894c204
Jadson Alves <jadson.almeida@escolar.ifrn.edu.br>
2025-06-06 18:35:02 -0300

    primitivos.py

--- primitivos.py
+++ primitivos.py
@@ -1,10 +1,16 @@
-n1 = int(input("digite um numero: "))
+"""n1 = int(input("digite um numero: "))
 n2 = int(input("digite outro numero: "))
 
 soma = n1 + n2
 
-print("soma entre {} e {} vale {}".format(n1, n2, soma))
+print("soma entre {} e {} vale {}".format(n1, n2, soma))"""
+#####FIm
 
-"""n = (input("digite algo: "))
-print(n.isnumeric())
-print(n.isalpha())"""
+"""a = (input("digite algo: "))
+print("O tipo primitivo desse valor é: ", type(a) )
+print("Só tem espaços?: ", a.isspace())
+print("É um numero?: ", a.isnumeric())
+print("É alfabeto?: ", a.isalpha())
+print("É alfanumerico?: ", a.isalnum())
+print("Esta em maiúsculas?: ", a.isupper())
+print("Está capitaizado?: ", a.istitle())"""
